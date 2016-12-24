1c94e0d13332ad9a894b051e262ae44975885329
Dustin <er.warahk@gmail.com>
2016-12-23 23:41:43 -0800

    Finished problem 27

--- problem27.py
+++ problem27.py
@@ -23,12 +23,43 @@ def testQuadratic(a,b,siv):
     '''
     count = 0
     while True:
-        n = count+1
+        n = count
         result = n*n + a*n + b
-        if result in siv:
+        if result > siv[-1]:
+            raise ValueError
+        if binarySearch(result, siv):
             count += 1
         else:
             return count
-    
+
+def binarySearch(value, siv):
+    '''check for value in siv using binary search'''
+    import bisect
+    i = bisect.bisect_left(siv, value)
+    if i != len(siv) and siv[i] == value:
+        return True
+    return False
+
 if __name__ == '__main__':
-    print(genPrimes(100))
+    import timeit
+    start_time = timeit.default_timer()
+    siv = genPrimes(20000)
+    elapsed = timeit.default_timer() - start_time
+    print("gen time: %s" % elapsed)
+    # loop through possible values for a and b coefficients
+    siv2 = [ x for x in siv if x < 1000 ]
+    siv2 += [ -x for x in siv2 ]
+    max = 0
+    best_a = 0
+    best_b = 0
+    start_time = timeit.default_timer()
+    for a in siv2:
+        for b in siv2:
+            count = testQuadratic(a, b, siv)
+            if count > max:
+                max = count
+                best_a = a
+                best_b = b
+    elapsed = timeit.default_timer() - start_time
+    print("a: %s; b: %s; count: %s; product: %s" % (best_a, best_b, max, best_a * best_b))
+    print("quad time: %s" % elapsed)
