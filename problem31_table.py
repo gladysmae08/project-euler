81a8490a4ac04ebcb3f08c7bb9775546d0ad1fe3
Dustin <er.warahk@gmail.com>
2017-02-21 01:27:52 -0800

    Removed unnecessary elif from problem31_table.py

--- problem31_table.py
+++ problem31_table.py
@@ -10,21 +10,17 @@ if __name__ == "__main__":
     for j in range(len(values)):
         for i in range(len(coins)):
             if coins[i] == 1:
-                print("i: %s, j: %s" % (i, j))
                 table[i][j] = 1
             else:
-                # Case 1: coin value greater than column value, 
-                #         take value from left cell
+                # Case 1: If coin value is greater than column value, 
+                #         take total from previous row's cell.
                 if coins[i] > values[j]:
                     table[i][j] = table[i-1][j]
-                # Case 2: (Not necessary, small optimization) If coin value
-                #         equals column value, total is prev denom total + 1
-                elif coins[i] == values[j]:
-                    table[i][j] = table[i][j-1] + 1
-                # Case 3: take values from previous denom's cell, and the value
-                #         from current denom row, in the current value - denom
-                #         value column
+                # Case 2: Else, sum totals from previous denom's cell (above),
+                #         and the total from current denom row, in the current
+                #         [value - denom] column (left).
                 else:
                     table[i][j] = table[i-1][j] + table[i][j - coins[i]]
+
     print("Time: %s" % (default_timer() - start))
     print("Ways to make change:", table[-1][-1])
