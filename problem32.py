dcee5f561f630d16b36800c12b9ca1208c0dacd8
Dustin <er.warahk@gmail.com>
2017-02-27 11:31:09 -0800

    Removed redundant elif's in problem32.py

--- problem32.py
+++ problem32.py
@@ -1,9 +1,11 @@
 from itertools import permutations
+from timeit import default_timer
 
 ints = [ str(x) for x in range(1,10) ]
 
 if __name__ == '__main__':
     products = []
+    start = default_timer()
     for perm in permutations(ints, 9):
         product = int(''.join(perm[-4:]))
         if product in products:
@@ -13,16 +15,14 @@ if __name__ == '__main__':
             products.append(product)
         elif int(''.join(perm[:2])) * int(''.join(perm[2:5])) == product:
             products.append(product)
-        elif int(''.join(perm[:3])) * int(''.join(perm[3:5])) == product:
-            products.append(product)
-        elif int(''.join(perm[:4])) * int(''.join(perm[4:5])) == product:
-            products.append(product)
         else:
             continue
     sum = 0
     for product in products:
         print(product)
         sum += product
+    time = default_timer() - start
+    print("Time: %s" % time)
     print("Sum: %s" % sum)
 
         
